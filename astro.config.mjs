8e58c9aa318761447cad105fb9fe9e7d9b467102
dachugden <jeff.cv09@gmail.com>
2024-02-27 03:01:28 -0500

    analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import alpinejs from "@astrojs/alpinejs";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()]
+  integrations: [tailwind(), alpinejs(), robotsTxt()],
+  site: "https://davidchugden.com/",
 });
